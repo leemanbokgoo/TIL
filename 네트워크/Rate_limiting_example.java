6e2a6c989854ce41f5ddb1d72cc8ff022bf96949
leemanbokgoo <dmsql5303@gmail.com>
2025-04-27 01:02:17 +0900

    주석 추가

--- 네트워크/Rate_limiting_example.java
+++ 네트워크/Rate_limiting_example.java
@@ -4,6 +4,11 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class SimpleRateLimiter {
+    /**
+     * 큐에는 최근 1초 동안 들어온 요청 시간만 저장.
+     * 새로운 요청이 오면, 오래된 기록을 빼내고, 현재 큐 크기를 검사.
+     * 200ms마다 요청을 시도해서 초당 5개 미만이면 요청을 허용하고, 아니면 거부.
+     */
     public static void main(String[] args) throws InterruptedException {
         Queue<Long> requestTimes = new LinkedList<>();
 
@@ -17,6 +22,7 @@ public class SimpleRateLimiter {
         while (requestCount-- > 0) {
             long now = System.currentTimeMillis();
 
+            // 만약 현재 시간(now) - 가장 오래된 시간이 1초(1000ms)보다 크면  타임윈도우에서 벗어난 요청이니까 큐에서 poll()로 꺼내서 제거.
             while (!requestTimes.isEmpty() && now - requestTimes.peek() > TIME_WINDOW_MS) {
                 requestTimes.poll();
             }
